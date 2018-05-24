0e7945eef3c8691187977f246ede433bd8e7ebd7
Henry Weng <henryweng2003@gmail.com>
2018-05-24 12:48:24 -0700

    Update Knight.java

--- FinalProject/src/FinalProject/Squares/Units/Knight.java
+++ FinalProject/src/FinalProject/Squares/Units/Knight.java
@@ -2,10 +2,10 @@ package FinalProject.Squares.Units;
 import FinalProject.Squares.*;
 import javax.swing.ImageIcon;
 
-public class Knight extends Troop {
+public class Horseman extends Troop {
     private ImageIcon redIcon = new ImageIcon(getClass().getResource("redKnight.png"));
     private ImageIcon blueIcon = new ImageIcon(getClass().getResource("blueStar.png"));
-    public Knight(int xPos, int yPos, boolean team){
+    public Horseman(int xPos, int yPos, boolean team){
         super(null, xPos, yPos, team);
         if (team){
             this.setIcon(redIcon);
@@ -13,14 +13,16 @@ public class Knight extends Troop {
             this.setIcon(blueIcon);
         }
     }
+    
     public boolean canMove(Square target){
         if(xDist(target) == 1 && yDist(target) == 2){
             return true;
-        } else if (xDist(target)==2&&yDist(target)==1) {
+        } else if (xDist(target)==2 && yDist(target)==1) {
             return true;
         }
         return false;
     }
+    
     public boolean canMove(int x, int y){
         Square target = new Square(null, x, y);
         return canMove(target);
